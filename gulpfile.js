c6dfe7d09c398c7f0b56a55ff8478be252af77b4
ALBERT <mac@MACs-MacBook-Pro.local>
2019-12-26 11:06:24 +0700

    Add up simple gulp release with n 13.2 and gulp 4.2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
 const gulp = require('gulp');
+const gulpCopy = require('gulp-copy');
+const rename = require('gulp-rename');
+const zip = require('gulp-zip');
+const path   = require("path");
+const fs     = require("fs");
+global.config = require("./gulp-config.json");
+
+gulp.task('release', function(cb){
+    gulp.src('./components/redshop/**')
+        .pipe(zip('redshop-v3.0.zip'))
+        .pipe(gulp.dest(config.releaseDir));
+    cb();
+});
