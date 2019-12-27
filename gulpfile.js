0cedb6982bde2b6c6e51180041ea8e21147242b6
ALBERT <mac@MACs-MacBook-Pro.local>
2019-12-27 09:35:04 +0700

    adjust gulp file, will be remove test.txt later

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-const gulp = require('gulp');
-const gulpCopy = require('gulp-copy');
-const rename = require('gulp-rename');
-const zip = require('gulp-zip');
-const path   = require("path");
-const fs     = require("fs");
-global.config = require("./gulp-config.json");
+const gulp      = require('gulp');
+const gulpCopy  = require('gulp-copy');
+const rename    = require('gulp-rename');
+const zip       = require('gulp-zip');
+const path      = require("path");
+const fs        = require("fs");
+global.config   = require("./gulp-config.json");
 
 gulp.task('release', function(cb){
     gulp.src('./components/redshop/**')
@@ -12,3 +12,20 @@ gulp.task('release', function(cb){
         .pipe(gulp.dest(config.releaseDir));
     cb();
 });
+
+/**
+ * GULP COPY
+ */
+gulp.task('copy:component:site', function(cb){
+    gulp.src(['./components/redshop/site/**'])
+        .pipe(gulp.dest(config.wwwDir + '/components/com_redshop'));
+    cb();
+});
+gulp.task('copy:component:admin', function(cb){
+    gulp.src(['./components/redshop/admin/**'])
+        .pipe(gulp.dest(config.wwwDir + '/administrator/components/com_redshop'));
+    cb();
+});
+gulp.task('copy:component', gulp.series('copy:component:admin', 'copy:component:site'));
+gulp.task('copy',gulp.series('copy:component'));
+
